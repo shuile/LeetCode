d3367e2aa597f90bf938c24ac1ba0a28b5cf3ba0
shuile <chenyiting1995@gmail.com>
2022-03-15 13:50:26 +0800

    second day of traning in codetop

--- src/cn/shui/codetop/client/L3.java
+++ src/cn/shui/codetop/client/L3.java
@@ -1,2 +1,49 @@
-package cn.shui.codetop.client;public class L3 {
+package cn.shui.codetop.client;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by chenyiting on 2022/3/15
+ * 无重复字符的最长子串
+ */
+public class L3 {
+
+    public static void main(String[] args) {
+        L3 test = new L3();
+        System.out.println(test.lengthOfLongestSubstring("abba"));;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                start = Math.max(start, map.get(ch) + 1);
+            }
+            map.put(ch, i);
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        Set<Character> set = new HashSet<>();
+        int n = s.length();
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                set.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !set.contains(s.charAt(rk + 1))) {
+                set.add(s.charAt(rk + 1));
+                rk++;
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
 }
--- src/cn/shui/codetop/client/L53.java
+++ src/cn/shui/codetop/client/L53.java
@@ -1,4 +1,64 @@
-package cn.shui.codetop.client;/**
+package cn.shui.codetop.client;
+
+/**
  * Created by chenyiting on 2022/3/15
- */public class L53 {
+ * 最大子数组和
+ */
+public class L53 {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, sum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (sum < 0) {
+                sum = num;
+            } else {
+                sum += num;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    // 动态规划
+    public int maxSubArray2(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    private static class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
+    // 分治
+    public int maxSubArray3(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+
+    private Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    private Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
 }
