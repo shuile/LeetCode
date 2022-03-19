5209f6d9e7ebe079de94d43a9c92eb200ce4b3f8
shuile <chenyiting1995@gmail.com>
2022-03-20 02:27:11 +0800

    sixth day of training in codetop

---
+++ src/cn/shui/codetop/client/L1.java
@@ -0,0 +1,32 @@
+package cn.shui.codetop.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by chenyiting on 2022/3/20
+ * 两数之和
+ */
+public class L1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return new int[0];
+    }
+}
