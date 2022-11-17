914101b4d26fbc65b32a610da25e0774a4796f57
shuile <chenyiting1995@gmail.com>
2022-11-17 23:09:58 +0800

    learning algorithm in order

---
+++ src/cn/shui/order/L292.java
@@ -0,0 +1,11 @@
+package cn.shui.order;
+
+/**
+ * Created by chenyiting on 2022/11/17
+ * Nim游戏
+ */
+public class L292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
