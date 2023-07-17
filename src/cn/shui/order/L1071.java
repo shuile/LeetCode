cf8202f3b090bd6a7b3eac56fc6d55708d44281d
shuile <chenyiting1995@gmail.com>
2023-07-17 23:03:59 +0800

    learning algorithm in order

---
+++ src/cn/shui/order/L1071.java
@@ -0,0 +1,23 @@
+package cn.shui.order;
+
+/**
+ * Created by chenyiting on 2023/7/17
+ */
+public class L1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!str1.concat(str2).equals(str2.concat(str1))) {
+            return "";
+        }
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    private int gcd(int a, int b) {
+        int remainder = a % b;
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            remainder = a % b;
+        }
+        return b;
+    }
+}
